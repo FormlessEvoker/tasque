e1d04f05a7864d105ff6ff4cc858317a28352d5a
Grayson Dubois <Grayson.Dubois@HCAHealthcare.com>
2025-06-26 09:05:29 -0500

    add tool versions and aliases

--- mix.exs
+++ mix.exs
@@ -5,9 +5,10 @@ defmodule Tasque.MixProject do
     [
       app: :tasque,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.18.3",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -21,10 +22,21 @@ defmodule Tasque.MixProject do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4"},
-      {:ex_doc, "~> 0.38.2"},
+      {:ex_doc, "~> 0.38.2", only: :dev, runtime: false, warn_if_outdated: true},
       {:git_hooks, "~> 0.8.1", only: [:dev], runtime: false},
       {:hammox, "~> 0.7", only: :test},
       {:mix_test_watch, "~> 1.3", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: [
+        "deps.get",
+        "git_hooks.install"
+      ],
+      test: ["test --color"],
+      docs: [""]
     ]
   end
 end
